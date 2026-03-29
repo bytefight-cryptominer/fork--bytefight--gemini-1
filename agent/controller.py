6d6c03927ff271f9798a3d929e90901ed9ee7dc9
Broyojo <dha@xoba.com>
2026-03-29 03:15:18 -0400

    v55: v52 + lower reserve (5 vs 10) for more aggressive painting

--- agent/controller.py
+++ agent/controller.py
@@ -71,8 +71,8 @@ class PlayerController:
             return count
 
         # --- Erase step for hill cells with opponent paint ---
-        # Distance 1: erase adjacent opponent hill cells
-        if stamina >= 55:
+        # Erase opponent-painted hill cells: both attacking (uncaptured) and defending (ours)
+        if stamina >= 55:  # 40 erase + 15 paint buffer
             for dr, dc in DR:
                 nr, nc = my_r + dr, my_c + dc
                 if not valid(nr, nc):
@@ -83,27 +83,6 @@ class PlayerController:
                     if nr == opp_r and nc == opp_c:
                         continue
                     return [Action.Move(DIR_MAP[(dr, dc)], move_type=MoveType.ERASE)]
-
-        # Distance 2: double-move to reach opponent hill cells 2 steps away
-        if stamina >= 60:
-            for dr, dc in DR:
-                nr, nc = my_r + dr, my_c + dc
-                if not valid(nr, nc):
-                    continue
-                if nr == opp_r and nc == opp_c:
-                    continue
-                if cell_owner(nr, nc) == self.opp and mdist(nr, nc, opp_r, opp_c) <= SAFE_DIST:
-                    continue
-                for dr2, dc2 in DR:
-                    nr2, nc2 = nr + dr2, nc + dc2
-                    if not valid(nr2, nc2):
-                        continue
-                    ecell2 = board.cells[nr2][nc2]
-                    if (ecell2.hill_id and ecell2.hill_id != 0 and
-                        ecell2.owner_parity == self.opp and
-                        (nr2, nc2) != (opp_r, opp_c)):
-                        return [Action.Move(DIR_MAP[(dr, dc)]),
-                                Action.Move(DIR_MAP[(dr2, dc2)], move_type=MoveType.ERASE)]
 
         # --- BFS ---
         best_first_dir = None
@@ -199,8 +178,8 @@ class PlayerController:
         if not valid(new_r, new_c):
             return actions
 
-        # Late game conservation
-        reserve = 25 if board.turn_count > 1400 else 10
+        # Paint budget
+        reserve = 20 if board.turn_count > 1400 else 5
         paint_budget = stamina - reserve
         paint_spent = 0
 
