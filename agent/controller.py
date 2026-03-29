cd82f33841b7fffc03ed1e0624a9cacc26b43409
Broyojo <dha@xoba.com>
2026-03-28 20:43:32 -0400

    v24: BFS depth 25 - testing even larger search radius

--- agent/controller.py
+++ agent/controller.py
@@ -112,7 +112,7 @@ class PlayerController:
 
         while queue:
             r, c, first_dir, depth = queue.popleft()
-            if depth > 20:
+            if depth > 25:
                 break
 
             cell = board.cells[r][c]
@@ -149,7 +149,7 @@ class PlayerController:
                 best_priority = priority
                 best_first_dir = first_dir
 
-            if depth < 20:
+            if depth < 25:
                 for dr, dc in DR:
                     nr, nc = r + dr, c + dc
                     if (nr, nc) in visited or not valid(nr, nc):
